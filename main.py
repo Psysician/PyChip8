6d08603bfda069407cb9d93bc4e31eb7a2198d8d
Max <maxfrank68@gmail.com>
2023-10-16 20:27:39 +0200

    i hate python

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ class Chip8:
         # stack pointer
         self.regSP = np.ubyte(0)
 
-        self.delayT = 0
-        self.soundT = 0
+        self.delayT = np.ubyte(0)
+        self.soundT = np.ubyte(0)
 
     def fetch(self) -> np.ushort:
         return (np.ushort(self.ram[self.regPC]) << 8) | self.ram[self.regPC + 1]
@@ -55,7 +55,7 @@ class Chip8:
 
                 # 1nnn jump to address
                 case x if (x & 0xF000) >> 12 == 1:
-                    self.regPC = np.ushort(x & 0xFFF)
+                    self.regPC = np.ushort(x & 0x0FFF)
 
                 # 2nnn call adress
                 case x if (x & 0xF000) >> 12 == 2:
@@ -105,13 +105,33 @@ class Chip8:
                 case x if (x & 0xF000) >> 12 == 5:
                     vX = x << 4 >> 12
                     vY = x << 8 >> 12
+                    valX = self.regV[vX]
+                    valY = self.regV[vY]
 
-                    op = x & 0xFFF0
+                    op = x & 0x000F
 
                     match op:
-                        case 0:
-                            pass
-                            
+                        case 0: # ass
+                            self.regV[vX] = self.regV[vY]
+                        case 1: # OR
+                            self.regV[vX] = self.regV[vX] | self.regV[vY]
+                        case 2: # AND
+                            self.regV[vX] = self.regV[vX] & self.regV[vY]
+                        case 3: # XOR
+                            self.regV[vX] = self.regV[vX] ^ self.regV[vY]
+                        case 4: # ADD
+                            res = int(valX) + int(valY)
+                            self.regV[0xF] = np.ubyte(1) if res > 0xFF else np.ubyte(0)
+                            self.regV[vX] = np.ubyte(res)
+
+                        case 5: # SUB
+                            self.regV[0xF] = np.ubyte(1) if valX > valY else np.ubyte(0)
+                            self.regV[vX] = valX - valY
+
+                        case 1: # SHR
+                            self.regV[0xF] = np.ubyte(1) if valX & 1 == 1 else np.ubyte(0)
+                            self.regV[vX] = np.ubyte()
+
                 # 9xy0 skip instrction if Vx != Vy
                 case x if (x & 0xF000) >> 12 == 9:
                     vX = x << 4 >> 12
@@ -124,6 +144,34 @@ class Chip8:
                 # Annn set reg I to nnn
                 case x if (x & 0xF000) >> 12 == 0xA:
                     self.regI = x << 4 >> 4
+
+                # Bnnn jmp to nnn + reg V
+                case x if (x & 0xF000) >> 12 == 0xB:
+                    self.regPC = np.ushort(x & 0x0FFF) + self.regV
+
+                # Cxkk Vx = random byte & kk
+                case x if (x & 0xF000) >> 12 == 0xC:
+                    rnd=np.ubyte(np.random.bytes(1)[0])
+                    vX = x << 4 >> 12
+                    self.regV[vX] = rnd & x << 8 >> 8
+
+                # Dxyn 
+                case x if (x & 0xF000) >> 12 == 0xD:
+                    pass
+                     
+                # Ex9E
+                case x if (x & 0xF0FF) == 0xE09E:
+                    pass
+
+                # ExA1
+                case x if (x & 0xF0FF) == 0xE0A1:
+                      pass
+                
+                case x if (x & 0xF0FF) == 0xF007:
+                    vX = x << 4 >> 12
+                    self.regV[vX] = self.delayT
+
+                
 
                 case x:
                     raise Exception("unimplemented: ", hex(x)[2:])
